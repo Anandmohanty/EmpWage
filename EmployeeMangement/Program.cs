1027219e7b5a054f458015ccdf4a9d9f0a058f73
Anandmohanty <mohantyanand3@gmail.com>
2023-01-09 19:39:34 +0530

    [Anand] - Checking Empage using Switch Case

--- EmployeeMangement/Program.cs
+++ EmployeeMangement/Program.cs
@@ -2,25 +2,27 @@
 {
     internal class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
-            int emphrs = 0;
+            int empHrs = 0;
             int empWage = 0;
+
             Random random = new Random();
-
-            int empCheck = random.Next(0, 2);
-
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
             {
-                emphrs = 8;
+                case IS_PART_TIME:
+                    empHrs = 4; break;
+
+                case IS_FULL_TIME:
+                    empHrs = 8; break;
+                default: empHrs = 0; break;
             }
-            else
-            {
-                emphrs = 0;
-            }
-            empWage = emphrs * EMP_RATE_PER_HOUR;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
 
         }
