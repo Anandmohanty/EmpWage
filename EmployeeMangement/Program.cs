3db1d1043f24f4d9bb7ff2ba5fa35bc1c237a8ac
Anandmohanty <mohantyanand3@gmail.com>
2023-01-10 18:04:01 +0530

    [Anand] - Uc6_EmpWWageOf100Days

--- EmployeeMangement/Program.cs
+++ EmployeeMangement/Program.cs
@@ -6,15 +6,16 @@
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_W0KING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
 
 
         static void Main(string[] args)
         {
-            int empHrs = 0, empWages = 0, totalEmpWage = 0;
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-            for (int day = 0; day < NUM_OF_W0KING_DAYS; day++)
+            while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < MAX_HRS_IN_MONTH)
             {
-
+                totalWorkingDays++;
                 Random randon = new Random();
                 int empCheck = randon.Next(0, 3);
 
@@ -29,14 +30,15 @@
                         empHrs = 0;
                         break;
                 }
-                empWages = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpHrs = totalEmpHrs + empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
 
-                totalEmpWage = totalEmpWage + empWages;
-                Console.WriteLine("Emp wage : " + empWages);
+
 
             }
-
+            int totalEmpWage = totalEmpHrs + EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+
 
 
 
