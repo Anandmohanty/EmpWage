8867abc9108e658bd540270cd74191a9e9aed2a7
Anandmohanty <mohantyanand3@gmail.com>
2023-01-10 17:50:51 +0530

    [Aanad] - Checking EmpWage Of Twenty Days

--- EmployeeMangement/Program.cs
+++ EmployeeMangement/Program.cs
@@ -5,25 +5,40 @@
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_W0KING_DAYS = 20;
+
 
         static void Main(string[] args)
         {
-            int empHrs = 0;
-            int empWage = 0;
+            int empHrs = 0, empWages = 0, totalEmpWage = 0;
 
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            for (int day = 0; day < NUM_OF_W0KING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4; break;
 
-                case IS_FULL_TIME:
-                    empHrs = 8; break;
-                default: empHrs = 0; break;
+                Random randon = new Random();
+                int empCheck = randon.Next(0, 3);
+
+                switch (empCheck)
+                {
+
+                    case IS_PART_TIME: empHrs = 4; break;
+
+                    case IS_FULL_TIME: empHrs = 8; break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWages = empHrs * EMP_RATE_PER_HOUR;
+
+                totalEmpWage = totalEmpWage + empWages;
+                Console.WriteLine("Emp wage : " + empWages);
+
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+
+
 
         }
     }
