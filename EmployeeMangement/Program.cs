734472f7b0f0e52e652c21694336984fedacc8ec
Anandmohanty <mohantyanand3@gmail.com>
2023-01-10 18:32:35 +0530

    [Anand] - Compute Employee Wage

--- EmployeeMangement/Program.cs
+++ EmployeeMangement/Program.cs
@@ -5,11 +5,10 @@
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_W0KING_DAYS = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         public const int MAX_HRS_IN_MONTH = 100;
 
-
-        static void Main(string[] args)
+        public static int computeEmpwage()
         {
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
@@ -38,6 +37,11 @@
             }
             int totalEmpWage = totalEmpHrs + EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            return totalEmpWage;
+        }
+        static void main(string[] args)
+        {
+            computeEmpwage();
 
 
 
